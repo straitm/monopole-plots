9a402adea3cc6254ada473039999274165cae0f1
Matthew Strait <mstrait@fnal.gov>
2020-06-26 19:57:06 -0500

    Try a rather different style for limit_plot

--- limit.cc
+++ limit.cc
@@ -11,8 +11,10 @@
 #include <TGraph.h>
 #include <TLegend.h>
 #include <TTree.h>
+#include <TStyle.h>
 
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 #include <vector>
@@ -163,78 +165,285 @@ void draw_limits(const lim_t & lims)
   g["half"] = new TGraph();
   g["full"] = new TGraph();
 
-  int n_point = 0;
-  for(const auto & lim : lims)
-  {
-    const Limit_Info & info = lim.second;
-
-    if (info.log_beta() > -3.75 && info.log_beta() < -1.05)
-    {
-      if(info.limit("full")*1e15 > 10)
-        printf("  $%3.1f$ & %02.0lf & %.0lf\\phantom{.0} \\\\\n",
-          info.log_beta(), info.limit("half")*1e15, info.limit("full")*1e15);
-      else
-        printf("  $%3.1f$ & %02.0lf & \\phantom{0}%.1lf \\\\\n",
-          info.log_beta(), info.limit("half")*1e15, info.limit("full")*1e15);
-
-      g.at("half")->SetPoint(n_point, info.log_beta(), info.limit("half"));
-      g.at("full")->SetPoint(n_point, info.log_beta(), info.limit("full"));
-      ++n_point;
+  std::ifstream infile("limitdata.txt");
+  if(infile.is_open()){
+    double logbeta, half, full;
+    while(infile >> logbeta >> half >> full){
+      g["half"]->SetPoint(g["half"]->GetN(), logbeta, half);
+      g["full"]->SetPoint(g["full"]->GetN(), logbeta, full);
     }
   }
+  else{
+    int n_point = 0;
+    std::ofstream outfile("limitdata.txt");
+    if(!outfile.is_open()){
+      fprintf(stderr, "Could not open limitdata.txt for writing\n");
+      exit(1);
+    }
+    for(const auto & lim : lims)
+    {
+      const Limit_Info & info = lim.second;
 
-  g.at("half")->SavePrimitive(std::cout);
+      if (info.log_beta() > -3.75 && info.log_beta() < -1.05)
+      {
+        if(info.limit("full")*1e15 > 10)
+          printf("  $%3.1f$ & %02.0lf & %.0lf\\phantom{.0} \\\\\n",
+            info.log_beta(), info.limit("half")*1e15, info.limit("full")*1e15);
+        else
+          printf("  $%3.1f$ & %02.0lf & \\phantom{0}%.1lf \\\\\n",
+            info.log_beta(), info.limit("half")*1e15, info.limit("full")*1e15);
+
+        g.at("half")->SetPoint(n_point, info.log_beta(), info.limit("half"));
+        g.at("full")->SetPoint(n_point, info.log_beta(), info.limit("full"));
+        ++n_point;
+
+        outfile << info.log_beta() << " " << info.limit("half") << " "
+                << info.limit("full") << std::endl;
+      }
+    }
+  }
 
   TCanvas *can = new TCanvas;
 
   can->SetCanvasSize(600, 400);
-  can->SetRightMargin(0.025);
+  can->SetRightMargin(0.015);
   can->SetTopMargin(0.03);
-  can->SetLeftMargin(0.14);
-  can->SetBottomMargin(0.14);
+  can->SetLeftMargin(0.13);
+  can->SetBottomMargin(0.13);
+
+  can->SetFrameLineWidth(2);
 
   can->SetLogy();
   can->SetTickx();
-  can->SetTicky();
-  
-  g.at("half")->Draw("AC");
-  g.at("full")->Draw("SAME C");
-  
-  g.at("half")->SetMarkerStyle(kOpenSquare);
-  g.at("full")->SetMarkerStyle(kFullSquare);
 
-  g.at("half")->SetLineStyle(kDashed);
-  g.at("full")->SetLineStyle(kSolid);
+  TH2D dum("dum", "", 1, -4.4, 0, 1, 1e-18, 1e-12);
+  
+  dum.Draw();
+  g.at("half")->Draw("lf");
+#ifdef DRAWHEAVY
+  g.at("full")->Draw("c");
+#endif
   
   g.at("half")->SetLineWidth(3);
-  g.at("full")->SetLineWidth(3);
+  g.at("half")->SetFillStyle(1001);
+  g.at("half")->SetFillColorAlpha(kBlack, 0.3);
+  g.at("full")->SetLineWidth(2);
 
-  TAxis *x = g.at("half")->GetXaxis();
+  TAxis *x = dum.GetXaxis();
   x->SetTitle("log_{10}(Monopole speed (#beta))");
-  x->SetTitleOffset(1.1);
   x->SetTitleSize(textsize);
   x->SetLabelSize(textsize);
   x->CenterTitle();
   x->SetNdivisions(6, 5, 0, true);
 
-  TAxis *y = g.at("half")->GetYaxis();
-  y->SetTitle("90% C.L. flux limit (cm^{#minus2}#kern[-0.5]{ }s^{#minus1}#kern[-0.5]{ }sr^{#minus1})");
+  TAxis *y = dum.GetYaxis();
+  y->SetTitle("90% C.L. flux limit (cm^{#minus2}#kern[-0.5]{ }"
+              "s^{#minus1}#kern[-0.5]{ }sr^{#minus1}#kern[-0.9]{ })");
   y->CenterTitle();
-  y->SetTitleOffset(1.2);
+  y->SetTitleOffset(1.15);
+  y->SetLabelOffset(0.001);
   y->SetTitleSize(textsize);
   y->SetLabelSize(textsize);
   y->SetRangeUser(1e-16, 1e-12);
 
-  TLegend *l = new TLegend(0.3, 0.27, 0.59, 0.41);
+  x->SetTickSize(0.025); // Smaller than default (0.03)
+  y->SetTickSize(0.025); 
+
+  TLegend *l = new TLegend(0.30,      0.57,
+                           0.30+0.15, 0.57 +0.12
+   #ifdef DRAWHEAVY
+    + 0.06
+   #endif
+  );
   l->SetTextSize(textsize);
   l->SetBorderSize(0);
   l->SetFillStyle(0);
   l->SetTextFont(42);
+  l->AddEntry((TH1D*)NULL, "This work", "");
   l->AddEntry
-    (g.at("half"), "m > 5 #times 10^{8}  GeV (#Omega = 2#pi)", "l");
+    (g.at("half"), " > 5#kern[-0.5]{ }#times#kern[-0.9]{ }10^{8}#kern[-0.05]{ }GeV",
+#ifdef DRAWHEAVY
+  "l");
   l->AddEntry
-    (g.at("full"), "m > 2 #times 10^{15} GeV (#Omega = 4#pi)", "l");
+    (g.at("full"), " > 2#kern[-0.5]{ }#times#kern[-0.9]{ }10^{15} GeV", "l");
+#else
+  "");
+#endif
   l->Draw();
+
+  TGraph slimlight;
+  slimlight.SetPoint(slimlight.GetN(),           0, 1e-11);
+  slimlight.SetPoint(slimlight.GetN(), log10(0.05), 1e-11);
+  slimlight.SetPoint(slimlight.GetN(), log10(0.05), 1.3e-15);
+  slimlight.SetPoint(slimlight.GetN(),           0, 1.3e-15);
+
+  slimlight.SetLineWidth(3);
+
+  slimlight.SetFillStyle(1001);
+  slimlight.SetFillColorAlpha(kGreen+2, 0.3);
+  slimlight.Draw("lf");
+ 
+  TGraph slimheavy;
+  slimheavy.SetPoint(slimheavy.GetN(), log10(0.05), 0.65e-15);
+  slimheavy.SetPoint(slimheavy.GetN(),           0, 0.65e-15);
+
+  slimheavy.SetLineWidth(2);
+
+#ifdef DRAWHEAVY
+  slimheavy.Draw("l");
+#endif
+
+  {
+    TLegend *l = new TLegend(0.59,      0.49,
+                             0.59+0.15, 0.49+0.12
+                             #ifdef DRAWHEAVY
+                               + 0.06
+                             #endif
+    );
+    l->SetTextSize(textsize);
+    l->SetBorderSize(0);
+    l->SetFillStyle(0);
+    l->SetTextFont(42);
+    l->AddEntry((TH1D*)NULL, "SLIM", "");
+    l->AddEntry(&slimlight, " > 10^{5} GeV",
+#ifdef DRAWHEAVY
+    "l");
+    l->AddEntry(&slimheavy, " > 5#kern[-0.5]{ }#times#kern[-0.9]{ }10^{13} GeV", "l");
+#else
+    "");
+#endif
+    l->Draw();
+  }
+
+  // Extracted from the plot in the MACRO paper
+  TGraph macroheavy;
+  macroheavy.SetPoint(macroheavy.GetN(), log10(4.e-05), 3.111271086997062e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(4.63003e-05), 2.7495747867546176e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(5.05009e-05), 2.559717811217955e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(6.11617e-05), 2.3293333163141835e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(8.0847e-05), 2.1544346900318954e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(9.05598e-05), 2.1128017944843972e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0001), 2.0991037201085632e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0001), 1.5843778311510706e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00011), 1.5869563145973281e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00011), 1.272095668992179e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00012), 1.2700287704932164e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00014), 1.29084978338489e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00018), 1.3597985579403563e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00018), 1.3356916613034106e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00020), 1.3686721650920103e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.000256597), 1.4701880261868488e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.000301783), 1.5792334259956746e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.000541903), 1.5588223004671087e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.000607082), 1.553760872996211e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.000735782), 1.5588223004671087e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0009235), 1.566445373014912e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0012), 1.5715481206644581e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0012), 1.4324301262444822e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00165697), 1.3911103503173643e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00298785), 1.3400427210994613e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00361065), 1.6234988203559148e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00404453), 1.5973122800602588e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.005), 1.5689946724208885e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.005), 1.6608827826277233e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.006), 1.63143817898912e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.00689217), 1.59212587731796e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0100762), 1.5237355654616932e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0203527), 1.4441241135747835e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0301526), 1.4116190614769622e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.044674), 1.3911103503173643e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0622653), 1.3820912681235753e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.0998089), 1.3776036785441396e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.100726), 1.4535480021259694e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.22115), 1.4488283956730566e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(0.659272), 1.44647434219323e-16);
+  macroheavy.SetPoint(macroheavy.GetN(), log10(1), 1.44647434219323e-16);
+
+  TGraph macrolight;
+  for(int i = 0; i < macroheavy.GetN(); i++)
+    macrolight.SetPoint(i, macroheavy.GetX()[i], macroheavy.GetY()[i]*2);
+
+  macrolight.SetPoint(macrolight.GetN(),
+    macroheavy.GetX()[macroheavy.GetN()-1], 1e-11);
+
+  macrolight.SetPoint(macrolight.GetN(),
+    macroheavy.GetX()[0], 1e-11);
+
+  macroheavy.SetLineWidth(2);
+
+  macrolight.SetLineWidth(2);
+
+#ifdef DRAWHEAVY
+  macroheavy.Draw("l");
+#endif
+
+  macrolight.SetFillStyle(1001);
+  macrolight.SetFillColorAlpha(kBlue, 0.3);
+  macrolight.Draw("lf");
+
+  {
+    TLegend *l = new TLegend(0.30,      0.32,
+                             0.30+0.15, 0.32+0.12
+                             #ifdef DRAWHEAVY
+                               + 0.06
+                             #endif
+                             );
+    l->SetTextSize(textsize);
+    l->SetBorderSize(0);
+    l->SetFillStyle(0);
+    l->SetTextFont(42);
+    l->AddEntry((TH1D*)NULL, "MACRO", "");
+    l->AddEntry(&macrolight, " > 10^{10} GeV",
+    #ifdef DRAWHEAVY
+    "l");
+    l->AddEntry(&macroheavy, " > 10^{16} GeV", "l");
+    #else
+    "");
+    #endif
+    l->Draw();
+  }
+
+  TGraph icecube;
+  icecube.SetPoint(icecube.GetN(), log10(0.995), 1e-11); // XXX
+  icecube.SetPoint(icecube.GetN(), log10(0.8), 1e-11); // XXX
+  icecube.SetPoint(icecube.GetN(), log10(0.8), 5.57e-18);
+  icecube.SetPoint(icecube.GetN(), log10(0.9), 3.56e-18);
+  icecube.SetPoint(icecube.GetN(), log10(0.995), 3.38e-18);
+  icecube.SetFillStyle(1001);
+  icecube.SetFillColorAlpha(kRed, 0.3);
+  icecube.Draw("lf");
+
+  icecube.SetLineWidth(3);
+  icecube.SetLineStyle(kSolid);
+
+  {
+    TLegend *l = new TLegend(0.76,      0.15,
+                             0.76+0.15, 0.15+0.12);
+    l->SetTextSize(textsize);
+    l->SetBorderSize(0);
+    l->SetFillStyle(0);
+    l->SetTextFont(42);
+    l->AddEntry((TH1D*)NULL, "IceCube", "");
+    l->AddEntry((TH1D*)NULL, "> 10^{6} GeV", "");
+    l->Draw();
+  }
+
+  slimlight     .SetLineStyle(kSolid  ); // 1e5
+  g.at("half")-> SetLineStyle(kSolid  ); // 5e8
+#ifdef DRAWHEAVY
+  macrolight    .SetLineStyle(7       ); // 1e10
+#endif
+  slimheavy     .SetLineStyle(kDashed ); // 5e13
+  g.at("full")-> SetLineStyle(kDotted ); // 2e15
+  macroheavy    .SetLineStyle(kDotted ); // 1e16 or 1e17
+
+  g.at("half")-> SetLineStyle(kSolid  ); // 5e8
+  g.at("full")-> SetLineStyle(kDotted ); // 2e15
+  slimlight     .SetLineColor(kGreen+2);
+  slimheavy     .SetLineColor(kGreen+2);
+  macrolight    .SetLineColor(kBlue);
+  macroheavy    .SetLineColor(kBlue);
+  icecube.SetLineColor(kRed);
 
   can->SaveAs("limit_plot.pdf");
 }
@@ -263,12 +472,12 @@ lim_t extract_limits(const std::string & file_name)
 
     tree->GetEntry(entry);
 
-#if 0
+    #if 0
     if (*input_file_name == "invalid"){
       std::cerr << "\n\n\t\tThe input_file_name branch is not set!\n" << std::endl;
       _exit(1);
     }
-#endif
+    #endif
 
     Event_Info e(t, *input_file_name);
     const double beta_f = e.beta_from_file_name();
@@ -287,8 +496,14 @@ lim_t extract_limits(const std::string & file_name)
 
 int main()
 {
+  gStyle->SetOptStat(0);
+ 
   std::map<std::string, lim_t> l;
-  l["0.9dEdx"] = extract_limits(MC_RECO_FILE);
+  {
+    std::ifstream infile("limitdata.txt");
+    if(!infile.is_open())
+      l["0.9dEdx"] = extract_limits(MC_RECO_FILE);
+  }
 
   draw_limits(l["0.9dEdx"]);
 }
